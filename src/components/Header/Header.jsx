f7d248e7e52ab04c9743a24e068b219a5e130e97
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2024-02-02 21:31:15 +0100

    Create header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,27 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/icons/logo.png';
+import shoppingBag from '../../assets/icons/shopping-bag.png';
 import './Header.scss';
 
 export const Header = () => {
   return (
     <header className="header">
-      <div>SmartCart</div>
+      <div className="logo">
+        <img src={logo} className="logo-icon" />
+        SmartCart
+      </div>
       <div className="nav-bar">
-        <Link to="/">Home</Link>
-        <Link to="/shop">Shop</Link>
-        <Link to="/about">About Us</Link>
-        <div>Cart</div>
+        <NavLink to="/" className="nav-link" activeClassName="active-link">
+          Home
+        </NavLink>
+        <NavLink to="/shop" className="nav-link" activeClassName="active-link">
+          Shop
+        </NavLink>
+        <NavLink to="/about" className="nav-link" activeClassName="active-link">
+          About Us
+        </NavLink>
+        <img src={shoppingBag} className="bag-icon" />
+        <span className="circle">0</span>
       </div>
     </header>
   );
