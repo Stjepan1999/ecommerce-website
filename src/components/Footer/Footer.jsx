d1f7e3a3cc58846118d797be223d3fb4fb5e0903
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2024-02-10 11:14:12 +0100

    Add styling to product page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,62 +7,64 @@ import xLogo from '../../assets/icons/x.png';
 
 export const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-section footer-section-wide">
-        <NavLink to="/" className="logo logo-secondary">
-          <img src={logoSecondary} className="logo-icon" />
-          SmartCart
-        </NavLink>
-        <p>Shop trendy accessories, clothes, and electronics online.</p>
-        <div className="icon-container">
-          <img src={facebookLogo} className="medium-icon" />
-          <img src={xLogo} className="medium-icon" />
-          <img src={instagramLogo} className="medium-icon" />
+    <footer className="footer-container">
+      <div className="footer">
+        <div className="footer-section footer-section-wide">
+          <NavLink to="/" className="logo logo-secondary">
+            <img src={logoSecondary} className="logo-icon" />
+            SmartCart
+          </NavLink>
+          <p>Shop trendy accessories, clothes, and electronics online.</p>
+          <div className="icon-container">
+            <img src={facebookLogo} className="medium-icon" />
+            <img src={xLogo} className="medium-icon" />
+            <img src={instagramLogo} className="medium-icon" />
+          </div>
         </div>
-      </div>
-      <div className="footer-section">
-        <h3 className="footer-heading">Products</h3>
-        <NavLink to="/" className="nav-link">
-          Air Force 1
-        </NavLink>
-        <NavLink to="/" className="nav-link">
-          Air Force 1
-        </NavLink>
-        <NavLink to="/" className="nav-link">
-          Air Force 1
-        </NavLink>
-        <NavLink to="/" className="nav-link">
-          Air Force 1
-        </NavLink>
-        <NavLink to="/" className="nav-link">
-          Air Force 1
-        </NavLink>
-        <NavLink to="/" className="nav-link">
-          Air Force 1
-        </NavLink>
-      </div>
-      <div className="footer-section">
-        <h3 className="footer-heading">Help</h3>
-        <NavLink to="/" className="nav-link">
-          About us
-        </NavLink>
-        <NavLink to="/" className="nav-link">
-          FAQs
-        </NavLink>
-        <NavLink to="/" className="nav-link">
-          How it works
-        </NavLink>
-        <NavLink to="/" className="nav-link">
-          Privacy policy
-        </NavLink>
-        <NavLink to="/" className="nav-link">
-          Payment policy
-        </NavLink>
-      </div>
-      <div className="footer-section">
-        <h3 className="footer-heading">Contact us</h3>
-        <p>info@smartcart.com</p>
-        <p>+28 3939 0023</p>
+        <div className="footer-section">
+          <h3 className="footer-heading">Products</h3>
+          <NavLink to="/" className="nav-link">
+            Air Force 1
+          </NavLink>
+          <NavLink to="/" className="nav-link">
+            Air Force 1
+          </NavLink>
+          <NavLink to="/" className="nav-link">
+            Air Force 1
+          </NavLink>
+          <NavLink to="/" className="nav-link">
+            Air Force 1
+          </NavLink>
+          <NavLink to="/" className="nav-link">
+            Air Force 1
+          </NavLink>
+          <NavLink to="/" className="nav-link">
+            Air Force 1
+          </NavLink>
+        </div>
+        <div className="footer-section">
+          <h3 className="footer-heading">Help</h3>
+          <NavLink to="/" className="nav-link">
+            About us
+          </NavLink>
+          <NavLink to="/" className="nav-link">
+            FAQs
+          </NavLink>
+          <NavLink to="/" className="nav-link">
+            How it works
+          </NavLink>
+          <NavLink to="/" className="nav-link">
+            Privacy policy
+          </NavLink>
+          <NavLink to="/" className="nav-link">
+            Payment policy
+          </NavLink>
+        </div>
+        <div className="footer-section">
+          <h3 className="footer-heading">Contact us</h3>
+          <p>info@smartcart.com</p>
+          <p>+28 3939 0023</p>
+        </div>
       </div>
     </footer>
   );
--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,3 +1,40 @@
+import './ProductPage.scss';
+import '../../styles.scss';
+import '../../components/Product/Product.scss';
+import starIcon from '../../assets/icons/star.png';
+
 export const ProductPage = () => {
-  return <div>This is product page</div>;
+  return (
+    <div className="page product-page">
+      <img
+        src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+        className="product-page-image"
+      />
+      <div className="product-info">
+        <div className="flex-container">
+          <p>
+            <span className="secondary-color-text">ID:</span> 10
+          </p>
+          <p>
+            <span className="secondary-color-text">Category:</span> Clothing
+          </p>
+        </div>
+        <h1>Mens Casual Premium Slim Fit T-Shirts</h1>
+        <div className="product-rating">
+          <img src={starIcon} className="rating-star" />
+          <span className="secondary-color-text">4.7</span> (319)
+        </div>
+        <h2>22.3 €</h2>
+        <hr />
+        <p className="text-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolore minima! Corporis eum voluptatum amet
+          sapiente! Adipisci, aut corporis nisi ab, a nam sed vel
+        </p>
+        <div className="flex-container">
+          <button className="button button-secondary">Go Back</button>
+          <button className="button button-primary">Add to cart</button>
+        </div>
+      </div>
+    </div>
+  );
 };
