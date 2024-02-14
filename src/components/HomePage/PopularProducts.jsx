da70187c845db85541a739554d56930ec7242e62
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2024-02-14 20:49:54 +0100

    Add most popular products to home page

--- src/components/HomePage/PopularProducts.jsx
+++ src/components/HomePage/PopularProducts.jsx
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom';
 import '../../pages/HomePage/HomePage.scss';
 import { Product } from '../Product/Product';
+import { useContext } from 'react';
+import { ShopContext } from '../../App';
 
 export const PopularProducts = () => {
+  const { products } = useContext(ShopContext);
+
+  const popularProducts = products.sort((a, b) => b.rating.rate - a.rating.rate).slice(0, 4);
+
   return (
     <div className="section popular-products-section">
       <h1 className="section-heading">
@@ -12,18 +18,19 @@ export const PopularProducts = () => {
         Elevate you lifestyle with our top-rated products. Experience quality and style with our selection.
       </p>
       <div className="products-container">
-        <Link to="/product" className="link">
-          <Product />
-        </Link>
-        <Link to="/product" className="link">
-          <Product />
-        </Link>
-        <Link to="/product" className="link">
-          <Product />
-        </Link>
-        <Link to="/product" className="link">
-          <Product />
-        </Link>
+        {popularProducts.map((product) => (
+          <Link to={`/product/${product.id}`} className="link" key={product.id}>
+            <Product
+              id={product.id}
+              category={product.category}
+              title={product.title}
+              description={product.description}
+              price={product.price}
+              rating={product.rating.rate}
+              image={product.image}
+            />
+          </Link>
+        ))}
       </div>
     </div>
   );
